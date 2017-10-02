c7e5837e7336b0a729709140b1c4a7ea0f273c82
Your Name <you@example.com>
2017-10-02 17:30:22 +0800

    up charset

--- db.php
+++ db.php
@@ -11,3 +11,10 @@ include_once "ezSQL/shared/ez_sql_core.php";
 include_once "ezSQL/mysqli/ez_sql_mysqli.php";
 
 $db = new ezSQL_mysqli( $db_user, $db_password, $db_name,'localhost');
+
+$db->query("SET CHARACTER SET utf8");
+$db->query("set session character_set_connection=utf8;");
+$db->query("set session character_set_results=utf8;");
+$db->query("set session character_set_client=utf8;");
+
+
