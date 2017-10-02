199b1ebf2d17f1ca1fd9492a945fea779c92d98d
JaeHo Song <thruthesky@gmail.com>
2017-10-02 16:20:17 +0800

    up

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
 
 $db_user = 'keywordobserver';
-$db_name = 'keyword-rank-observer';
+$db_name = 'keyword_rank_observer';
 $db_password = 'Mon3:56PM';
 
 
 
 include_once "ezSQL/shared/ez_sql_core.php";
 include_once "ezSQL/mysqli/ez_sql_mysqli.php";
-$db = new ezSQL_mysqli( $db_user, $db_password,'keyword-rank-observer','localhost');
+$db = new ezSQL_mysqli( $db_user, $db_password,'keyword_rank_observer','localhost');
 
