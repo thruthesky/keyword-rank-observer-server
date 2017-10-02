f6ac637b813bf842515b1cc8c9b1d52cfd626553
Your Name <you@example.com>
2017-10-02 17:03:59 +0800

    up

--- db.php
+++ db.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(E_ERROR | E_PARSE );
 
 $db_user = 'keywordobserver';
 $db_name = 'keyword_rank_observer';
@@ -9,6 +10,4 @@ $db_password = 'Mon3:56PM';
 include_once "ezSQL/shared/ez_sql_core.php";
 include_once "ezSQL/mysqli/ez_sql_mysqli.php";
 
-$old_error_level = error_reporting(0);
 $db = new ezSQL_mysqli( $db_user, $db_password, $db_name,'localhost');
-error_reporting( $old_error_level );
--- query.php
+++ query.php
@@ -1,2 +1,7 @@
 <?php
 
+
+include 'db.php';
+$rows = $db->get_results("SELECT * FROM keyword_ranks");
+print_r($rows);
+
