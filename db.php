7962113171b8adaa6c00bd44c455662c6a2c664b
Your Name <you@example.com>
2017-10-02 16:19:41 +0800

    updateing db

--- db.php
+++ db.php
@@ -8,5 +8,5 @@ $db_password = 'Mon3:56PM';
 
 include_once "ezSQL/shared/ez_sql_core.php";
 include_once "ezSQL/mysqli/ez_sql_mysqli.php";
-$db = new ezSQL_mysqli( $db_user, $db_password,'keyword-rank-observer','localhost');
+$db = new ezSQL_mysqli( $db_user, $db_password, $db_name,'localhost');
 
